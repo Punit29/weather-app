2a99d2ebcac22cc6539b67c24ccd1f4e8478f08f
Punit <punitpundir0@gmail.com>
2024-01-03 13:19:02 +0530

    added tailwind and new design

--- src/components/WeatherApp/index.js
+++ src/components/WeatherApp/index.js
@@ -1,33 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { toast, ToastContainer } from 'react-toastify';
-  import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import "./index.css";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
 
-import searchIcon from "../images/search.png";
-import cloudIcon from "../images/cloud.png";
-import rainIcon from "../images/rain.png";
-import snowIcon from "../images/snow.png";
-import clearIcon from "../images/clear.png";
-import drizzleIcon from "../images/drizzle.png";
-import windIcon from "../images/wind.png";
-import humidityIcon from "../images/humidity.png";
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+import { TbTemperatureCelsius } from "react-icons/tb";
 
 const API_KEY = "c5a6f36df2e854825cf78b60209a47f9";
 
 const iconMap = {
-    Rain: rainIcon,
-    Clouds: cloudIcon,
-    Clear: clearIcon,
-    Snow: snowIcon,
-    Drizzle: drizzleIcon,
-}
+  Rain: <IoMdRainy />,
+  Clouds: <IoMdCloudy />,
+  Haze: <BsCloudHaze2Fill />,
+  Clear: <IoMdSunny />,
+  Snow: <IoMdSnow />,
+  Drizzle: <BsCloudDrizzleFill />,
+  Thunderstorm: <IoMdThunderstorm />,
+};
 
 const WeatherApp = () => {
-  const [location, setLocation] = useState(localStorage.getItem('lastCity') ?? "London");
+  const [location, setLocation] = useState(
+    localStorage.getItem("lastCity") ?? "London"
+  );
   const [weather, setWeather] = useState({});
-  const [icon, setIcon] = useState('');
+  const [icon, setIcon] = useState("");
 
   useEffect(() => {
     search();
@@ -36,7 +48,7 @@ const WeatherApp = () => {
   const search = async () => {
     if (!location.length) {
       toast.error("Please enter location", {
-        position: toast.POSITION.TOP_CENTER
+        position: toast.POSITION.TOP_CENTER,
       });
       return;
     }
@@ -46,55 +58,112 @@ const WeatherApp = () => {
       )
       .then((res) => {
         setWeather({
-          humidity: res.data.main.humidity + "%",
-          wind: res.data.wind.speed + "m/s",
-          temperature: Math.floor(res.data.main.temp) + "°C",
+          humidity: res.data.main.humidity,
+          wind: res.data.wind.speed,
+          temperature: Math.floor(res.data.main.temp),
           location: res.data.name,
+          country: res.data.sys.country,
+          description: res.data.weather[0].description,
+          feelsLike: Math.floor(res.data.main.feels_like),
+          visibility: res.data.visibility,
         });
-        localStorage.setItem('lastCity', res.data.name);
-        setIcon(iconMap[res.data.weather[0].main] || iconMap.Clouds)
+        localStorage.setItem("lastCity", res.data.name);
+        setIcon(iconMap[res.data.weather[0].main] || iconMap.Clouds);
       })
-      .catch(err => {
+      .catch((err) => {
         toast.error(err.response.data.message, {
-            position: toast.POSITION.TOP_CENTER
-          });
+          position: toast.POSITION.TOP_CENTER,
+        });
       });
   };
 
+  const date = new Date();
+
   return (
-    <div className="container">
-      <form className="top" onSubmit={(e) => {
-        e.preventDefault();
-        search()}}>
-        <input
-          type="text"
-          className="input"
-          placeholder="Search"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <div className="search-icon" onClick={() => search()}>
-          <img className="search-image" src={searchIcon} alt="search-icon" />
+    <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0">
+      <form
+        className="h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          search();
+        }}
+      >
+        <div className="h-full relative flex items-center justify-between p-2">
+          <input
+            type="text"
+            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
+            placeholder="Search"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <button className="bg-[#1ab8ed] w-20 h-12 rounded-full flex justify-center items-center transition" onClick={() => search()}>
+            <IoMdSearch className="text-2xl text-white" />
+          </button>
         </div>
       </form>
-      <div className="weather-image">
-        <img src={icon} alt="cloud icon" className="weather" />
-      </div>
-      <div className="temperature">{weather?.temperature}</div>
-      <div className="location">{weather?.location}</div>
-      <div className="details-wrapper">
-        <div className="detail">
-          <img src={humidityIcon} alt="" className="icon" />
-          <div className="data">
-            <div className="humidity">{weather?.humidity}</div>
-            <div className="text">Humidity</div>
+      <div className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6">
+        <div className="flex items-center gap-x-5">
+          <div className="text-[87px]">{icon}</div>
+          <div className="text-2xl font-semibold">
+            {weather.location}, {weather.country}
+            <div>
+              {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+              {date.getUTCFullYear()}
+            </div>
           </div>
         </div>
-        <div className="detail">
-          <img src={windIcon} alt="" className="icon" />
-          <div className="data">
-            <div className="humidity">{weather?.wind}</div>
-            <div className="text">Wind Speed</div>
+        <div className="my-20">
+          <div className="flex justify-center items-center">
+            <div className="text-[144px] leading-none font-light">
+              {weather?.temperature}
+            </div>
+            <div className="text-4xl">
+              <TbTemperatureCelsius />
+            </div>
+          </div>
+          <div className="capitalize text-center">{weather.description}</div>
+        </div>
+        <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-x-2">
+              <div className="text-[20px]">
+                <BsEye />
+              </div>
+              <div>
+                Visibility
+                <span className="ml-2">{weather.visibility / 1000} km</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-x-2">
+              <div className="text-[20px]">
+                <BsThermometer />
+              </div>
+              <div className="flex">
+                Feels like{" "}
+                <div className="flex ml-6">
+                  {weather.feelsLike}
+                  <TbTemperatureCelsius />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-x-2">
+              <div className="text-[20px]">
+                <BsWater />
+              </div>
+              <div>
+                Humidity <span className="ml-2">{weather.humidity} %</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-x-2">
+              <div className="text-[20px]">
+                <BsWind />
+              </div>
+              <div>
+                Wind <span className="ml-2">{weather.wind} m/s</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      primary: 'Poppins',
+    },
+    extend: {
+      backgroundImage: {
+        gradientBg: "url('/src/components/images/bg.png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
